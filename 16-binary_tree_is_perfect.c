f07f4abb9350ae19d47f6a3332f8175496811fb3
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-07-20 10:13:20 +0100

    fix tree is perf file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_is_perfect - function that says if a tree is perfect or not
  * it has to be the same quantity of levels in left as right, and also
@@ -7,7 +6,6 @@
  * @tree: tree to check
  * Return: 0 if is not a perfect or other number that is the level of height
  */
-
 int tree_is_perfect(const binary_tree_t *tree)
 {
 	int l = 0, r = 0;
@@ -29,13 +27,11 @@ int tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	}
 }
-
 /**
  * binary_tree_is_perfect - perfect or not a tree
  * @tree: tree to check
  * Return: 1 is it is or 0 if not
  */
-
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int result = 0;
