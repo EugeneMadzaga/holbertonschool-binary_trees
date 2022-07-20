b775428ad58fc37300dab5c30bc27c1d62cccce9
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-07-20 09:23:38 +0100

    push 2 file

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - insert node as the left child of another node
+ * @parent: parent node
+ * @value: value of the new node
+ * Return: a pointer to the new node or NULL
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *tmpNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+
+	if (tmpNode == NULL)
+		return (NULL);
+
+	tmpNode->n = value;
+	tmpNode->parent = parent;
+	tmpNode->left = NULL;
+	tmpNode->right = NULL;
+
+	if (!value)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		tmpNode->right = parent->right;
+		parent->right->parent = tmpNode;
+	}
+	parent->right = tmpNode;
+	return (tmpNode);
+}
